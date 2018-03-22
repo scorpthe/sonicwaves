008ce90ef14a953aa7a0c992b3718caf295777d9
Ilya Bushuev <scorpthe@ya.ru>
2018-03-22 17:47:48 +0300

    Sounder: 0.03 Changes: * playFile, * playMusic, # getGamma Written: 16.03.2018 3:55

--- sounder.py
+++ sounder.py
@@ -1,168 +1,125 @@
+"""
+version 0.8 E4:8*3
+
+A4 in scientific notation = 440.0 Hz
+Then C0 = 16.352 Hz
+"""
+
 from winsound import Beep
 
+NOTE_AMOUNT = 12
+A4 = (440.00, (12*4 + 9))
+NOTE_ID = {'C':0, 'C#':1, 'Db':1, 'D':2, 'D#':3, 'Eb':3, 'E':4,
+           'F':5, 'F#':6, 'Gb':6, 'G':7, 'G#':8, 'Ab':8, 'A':9,
+           'A#':10, 'Hb':10, 'H':11}
+MS_IN_MIN = 60 * 1000 # SecInMin * MsInSec
+BPM = 120 #beats (quarter note) per minute
+QTR = 4
+
 class Sounder:
-    """ BASICS """
-    NoteAmount = 12
-    A4 = (440.00, (12*4 + 9))
-    # A4(hz, number) in scientific notation
-    # C0 = 16.352 Hz
-    NoteID = {'C':0, 'C#':1, 'Db':1, 'D':2, 'D#':3, 'Eb':3, 'E':4,
-              'F':5, 'F#':6, 'Gb':6, 'G':7, 'G#':8, 'Ab':8, 'A':9,
-              'A#':10, 'Hb':10, 'H':11}
-    MsInSec = 1000
-    SecInMin = 60
-    MsInMin = SecInMin * MsInSec
-    StdTempo = 120 #beat (quarter note) per minute
-
-    bpm = StdTempo
-    quarter = MsInMin // bpm
-    whole = quarter * 4
-    div = 8
+    def defMsWhole(self, bpm):
+        return MS_IN_MIN // bpm * QTR
+        
+    def defStDiv(self, div):
+        return div
     
-    #tact = [('C4', 1/2), ('D4', 1/2)]
-    #sum = 0
-    #for el in tact: sum += el[1]
-    #sum == 1
-
-    #in file:
-    #s = 'bpm = 120'
-    #int(s.split('=')[1])
+    def __init__(self):
+        self.msWhole = self.defMsWhole(BPM)
+        self.stDiv = self.defStDiv(QTR)
     
-    #Notes12 = ('C','CD','D','DE','E','F','FG','G','GA','A','AH','H')
-    
-    #Major = (2,2,1,2,2,2,1)
-    #Minor = (2,1,2,2,1,2,2)
-
-    def _setTempo(self, tempo):
-        self.tempo = tempo
-        self.quarter = self.MsInMin // self.tempo
-        self.whole = self.quarter * 4
-
-    def _setDiv(self, div):
-        self.div = div
-
-    def _noteNumByName(self, name='C0'):
+    def _noteNumByName(self, name):
         try:
             octave = int(name[-1])
-            note = self.NoteID[name[:-1]]
-            res = self.NoteAmount*octave + note
+            note = NOTE_ID[name[:-1]]
+            res = NOTE_AMOUNT*octave + note
         except Exception: #KeyError, ValueError, IndexError...
             res = False
         return res
 
-    def _noteHzByNum(self, num=0):
-        return self.A4[0] * (2 ** ((num - self.A4[1])/12))
+    def _noteHzByNum(self, num):
+        return A4[0] * (2 ** ((num - A4[1])/12))
 
-    def noteHzByName(self, name='A4'):
+    def noteHzByName(self, name):
         num = self._noteNumByName(name)
         return self._noteHzByNum(num)
 
-    def playNoteMs(self, name='C4', ms=1000):
+    def _playNoteMs(self, name, ms):
         hz = self.noteHzByName(name)
         Beep(round(hz), ms)
 
-    def playNoteDur(self, name='C4', dur=1):
+    def playNoteDur(self, name, dtr, ntr):
         hz = self.noteHzByName(name)
-        ms = int(self.whole * dur)
+        div = ntr/dtr
+        ms = int(self.msWhole * div)
         Beep(round(hz), ms)
 
-    def playMusic(self, music=[]):
+    def playMusic(self, music):
         for note in music:
             print(note)
-            self.playNoteDur(note[0], note[1])
+            self.playNoteDur(note[0], note[1], note[2])
+
+    def _readFile(self, name):
+        text = ''
+        with open(name,'r') as file:
+            text = file.read()
+        print(text)
+        text = text.replace(' ','')
+        text = text.replace('\n',',')
+        expr = text.split(',')
+        for el in expr:
+            if el == '':
+                del el
+        return expr
+
+    def _defActs(self, expr=[]):
+        acts = []
+        for el in expr:
+            if el == '':
+                pass
+            elif el.find('bpm=') == 0:
+                split = el.split('=')
+                acts.append(tuple([split[0], int(split[1])]))
+            elif el.find('div=') == 0:
+                split = el.split('=')
+                acts.append(tuple([split[0], int(split[1])]))
+            else:
+                el = el.replace('*',' ')
+                el = el.replace(':',' ')
+                split = el.split(' ')
+                for i in range(len(split)):
+                    if i > 0:
+                        split[i] = int(split[i])
+                acts.append(tuple(split))
+        return acts
+
+    def _playActs(self, acts=[]):
+        for el in acts:
+            if el[0] == 'bpm':
+                bpm = el[1]
+                self.msWhole = self.defMsWhole(bpm)
+                print('tempo set to', bpm)
+            elif el[0] == 'div':
+                div = el[1]
+                self.stDiv = self.defStDiv(div)
+                print('div set to', div)
+            else:
+                div = self.stDiv
+                dur = 1
+                note = el[0]
+                if len(el) == 3:
+                    div = el[1]
+                    dur = el[2]
+                elif len(el) == 2:
+                    div = el[1]
+                self.playNoteDur(note,div,dur)
 
     def playFile(self, name):
-        lines = []
-        with open(name,'r') as file:
-            lines = file
-            #for line in file:
-            #    lines.append(line)
-        print(lines)
-        phrases = []
-        for i in range(len(lines)):
-            lines[i] = lines[i].replace(' ','')
-            lines[i] = lines[i].replace('\n','')
-            #lines[i].split(',')
-            for el in phrases:
-                if el.find('bpm=') == 0:
-                    self._setTempo(int(el.split('=')[1]))
-                    print('tempo set to', self.tempo)
-                elif el.find('div=') == 0:
-                    self._setDiv(int(el.split('=')[1]))
-                    print('div set to', self.div)
-        print(phrases)
-
-"""    
-    def getNoteByName(self, note):
-        failed = False
-        try:
-            note = self.Notes12.index(note)
-        except ValueError:
-            failed = True
-        note = note if not failed else -1
-        return note
-
-    def getToneByName(self, tone):
-        if tone == 'major':
-            tone = self.Major
-        elif tone == 'minor':
-            tone = self.Minor
-        else:
-            tone = -1
-        return tone
-
-    def getDiBeNote(self, curNote, prevNote):
-        if len(curNote) != len(prevNote):
-            
-            curNote = curNote[0]+'#'
-        return curNote
-
-    def getDiBeGamma(self, oldGamma):
-        gamma = []
-        for i in range(len(oldGamma)):
-            gamma.append(self.getDiBeNote(oldGamma[i], oldGamma[i-1]))
-        return gamma
-    
-    def getGamma(self, note='C', tone='major'):
-        gamma = []
-        failed = 0
-        # Get note
-        note = self.getNoteByName(note)
-        if note == -1:
-            failed += 1
-        # Get tone
-        tone = self.getToneByName(tone)
-        if tone == -1:
-            failed += 1
-        # If not failed
-        if not failed:
-            ln = len(self.Notes12)
-            cur = note
-            for i in tone:
-                gamma.append(self.Notes12[cur%ln])
-                cur += i
-            #gamma = self.getDiBeGamma(gamma)
-        res = gamma if not failed else not failed
-        return res
-"""
-
-
-
-
-
-
+        expr = self._readFile(name)
+        print(expr, '\n\n')
+        acts = self._defActs(expr)
+        print(acts, '\n\n')
+        self._playActs(acts)
 
 snd = Sounder()
-"""
-cmaj = snd.getGamma('CD','minor')
-print(cmaj)
-print(snd.noteHzByName('D4'), round(snd.noteHzByName('D4')))
-snd._setTempo(60)
-music = [('E4',1/8*3), ('E4',1/16), ('F#4',1/16),
-         ('G4',1/8), ('F#4',1/16), ('E4',1/16),
-         ('D4',1/8), ('E4',1/16), ('F#4',1/16),
-         ('E4',1/8*3), ('D4',1/16), ('E4',1/16),
-         ('D4',1/8*3), ('H3',1/16), ('D4',1/16)]
-snd.playMusic(music)
-"""
-snd.playFile('music')
+#snd.playFile('music')
+snd._readFile('music')
